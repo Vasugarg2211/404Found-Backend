01a01e7c63ffe7a1e798fb3c3d28283a9f29b29a
Prranav <prranavbabbar2317@gmail.com>
2024-09-28 10:44:34 +0530

    initial set up

---
+++ src/main/java/com/__found/found/logs/FoundLogsApplication.java
@@ -0,0 +1,13 @@
+package com.__found.found.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundLogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/__found/found/logs/FoundLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__found.found.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
