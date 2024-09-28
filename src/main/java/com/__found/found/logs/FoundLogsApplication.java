3a7c386c8a1f99c842280c185d61db76c5e58413
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2024-09-28 11:01:15 +0530

    Dependencies added

--- src/main/java/com/__found/found/logs/FoundLogsApplication.java
+++ src/main/java/com/__found/found/logs/FoundLogsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoundLogsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Log Application");
 		SpringApplication.run(FoundLogsApplication.class, args);
 	}
 
